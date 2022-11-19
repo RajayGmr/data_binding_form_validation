14bb79bb3e16ccfe4c10faedc26aaae4803a26a3
rajayGmr <rajayghimire1999@gmail.com>
2022-11-19 22:11:32 +0700

    form validation & data binding

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Form</h1>
+  <signup></signup>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import signupform from "./components/Signupform.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    signup:signupform,
+    
   }
 }
 </script>
@@ -22,5 +23,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  background-color: blanchedalmond;
+  margin: 0;
 }
 </style>
---
+++ src/components/Signupform.vue
@@ -0,0 +1,164 @@
+<template>
+  <form action="" @submit.prevent="submitform">
+    <label for="">Email</label>
+    <input type="email" required v-model="email">
+    <label for="">Password</label>
+    <input type="password" required v-model="password">
+    <p v-if="errorMsg" class="theme">{{errorMsg}}</p>
+
+    <label for="">Role:</label>
+    <select name="" id="" v-model="role">
+        <option value="developer">Web Developer</option>
+        <option value="designer">Web Designer</option>
+    </select>
+
+    <label for="">Skill</label>
+    <input type="text" @keyup.alt="addSkill" v-model="skill">
+    <div v-for="ski in skills" :key="ski">
+    <p> {{ski}} <span class="cross" @click="deleteSkill(ski)">&#x2716;</span></p>
+  </div>
+
+    <div>
+        <input type="checkbox" id="checkbox"  v-model="check">
+        <label for="checkbox">Accept Terms and Conditions</label>
+    </div>
+     
+    <!-- Multiple CheckBox -->
+    <div>
+        <label for="">Choose Location</label><br>
+        <input type="checkbox" id="checkbox1" value="Bangkok" v-model="click">
+        <label for="checkbox1">Bangkok</label>
+        <input type="checkbox" id="checkbox2" value="Phuket" v-model="click">
+        <label for="checkbox2">Phuket</label>
+        <input type="checkbox" id="checkbox3" value="Chaing Mai" v-model="click">
+        <label for="checkbox3">Chaing Mai</label>
+        <input type="checkbox" id="checkbox4" value="Chaing Rai" v-model="click">
+        <label for="checkbox4">Chaing Rai</label>
+    </div>
+
+    <!-- radio check box -->
+    <div>
+      <label for="">Experience Year</label><br>
+      <input type="radio" name="" id="0-1" value="0-1" v-model="yearOfExperience">
+      <label for="0-1">1 year</label>
+      <input type="radio" name="" id="3-5" value="3-5" v-model="yearOfExperience">
+      <label for="3-5">3 years</label>
+      <input type="radio" name="" id="5+" value="5+" v-model="yearOfExperience">
+      <label for="5+">5 years</label>
+    </div>
+
+    <div class="align">
+        <button class="create">Create Account</button>
+    </div>
+    
+  </form>
+  <p>Email - {{email}}</p>
+  <p>Password - {{password}}</p>
+  <P>Role - {{role}}</P>
+  <p>Accpet - {{check}}</p>
+  <p>Location - {{click}}</p>
+  <p>Year of Experience - {{yearOfExperience}}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email: "",
+            password:"",
+            role:"",
+            skills:[],
+            skill:"",
+            check: false,
+            click: [],
+            yearOfExperience: "",
+            errorMsg:"",
+        }
+    },
+    methods:{
+        addSkill(e){
+            if(e.key==="," && this.skill){
+                this.skills.push(this.skill);   
+                this.skill="";
+            } 
+        },
+        deleteSkill(ski){
+            this.skills=this.skills.filter(loopski=>{
+                return loopski!=ski;
+            });
+        },
+        submitform(){
+            if(this.password.length<8){
+                this.errorMsg="Password Must be at least 8 characters";
+            }else{
+                this.errorMsg="";
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+form{
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius:10px;
+}
+label{
+    color: rgb(0, 0, 0);
+    display: inline-block;
+    /* margin: 25px 0 15px; */
+    margin: 10px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input,select{
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: rgb(85, 85, 85);;
+}
+input[type="checkbox"]{
+    display: inline-block;
+    width: 16px;
+    margin: 0 5px 0 0;
+    position: relative;
+    top: 2px;  
+}
+input[type="radio"]{
+    display: inline-block;
+    width: 16px;
+    margin: 0 5px 0 0;
+    position: relative;
+    top: 2px;  
+}
+.cross{
+    cursor: pointer;
+    color: red;
+    margin-left: 400px;
+}
+.create{
+    background-color: steelblue;
+    padding: 8px;
+    margin-top: 10px;
+    color: white;
+    border-radius: 10px;
+}
+.align{
+    text-align: center;
+}
+.theme{
+    color: red;
+}
+
+
+</style>
